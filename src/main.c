7b13e232e14298cd04fe1dff4b395fd0018c32a0
root <root@localhost>
2024-10-03 23:45:47 +0300

    auto publish 1727988347804

---
+++ src/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+    int Book_ID, Due_date, Return_date,Days_overdue;
+    
+    printf("Enter the book ID:");
+    scanf("%d",&Book_ID);
+    
+    printf(" Enter the due date:");
+    scanf("%d",&Due_date);
+    
+    printf("Enter the return date:");
+    scanf("%d",&Return_date);
+    
+    printf("days overdue = returnDate-dueDate:");
+    scanf(" %d",&Days_overdue);
+     return 0;
+     }
+     
+    
+    
